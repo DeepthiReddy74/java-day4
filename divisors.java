84dc8ab4214bfe0c9e54f6a1bf5fdb115963ea15
DeepthiReddy74 <reddydeepthi68699@gmail.com>
2024-12-05 13:33:46 +0530

    Add files via upload

---
+++ divisors.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class divisors
+{
+    public static void main(String args[])
+{
+    Scanner Sc=new Scanner(System.in);
+    int t=Sc.nextInt();
+    for(int i=0;i<t;i++)
+    {
+        int n=Sc.nextInt();
+        for(int j=1;j<=n;j++)
+        {
+            if(n%j==0)
+            System.out.println(j+" ");
+        }
+        System.out.println();
+    }
+}
+} 
