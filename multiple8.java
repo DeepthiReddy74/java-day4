5555f7c185a8beb9a2b3e541ffc496f13ac8329a
DeepthiReddy74 <reddydeepthi68699@gmail.com>
2024-12-05 13:34:58 +0530

    Add files via upload

---
+++ multiple8.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class multiple8
+{
+    public static void main(String args[])
+{
+    Scanner Sc=new Scanner(System.in);
+    int t=Sc.nextInt();
+    for(int i=0;i<t;i++)
+    {
+        int n=Sc.nextInt();
+        if((n&7)==0)
+        {
+            System.out.println("multiple of 8");
+        }
+        else
+        {
+            System.out.println("not multiple of 8");
+        }
+        }
+        }
+    }
